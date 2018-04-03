76f5781c605abbafc96ad8919def82a4dbf3a0fa
Bob Brown <robertbernardbrown@gmail.com>
2018-04-03 17:25:23 -0400

    sets up scss and js watching in gulp file

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+console.log("yoyoyo");
+console.log("you rock");
+console.log("poop");
+
+var headerNav = document.getElementsByClassName("header-nav")
+console.log(headerNav);
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require("gulp");
+    uglify = require("gulp-uglify");
+    rename = require("gulp-rename");
+    sass = require('gulp-sass');
+
+
+gulp.task("scripts", function(){
+    gulp.src(["assets/js/**/*.js","!assets/js/**/*.min.js"])
+    .pipe(rename({suffix:".min"}))
+    .pipe(uglify())
+    .pipe(gulp.dest("assets/js"));
+})
+
+gulp.task('scss', function () {
+    gulp.src(['assets/scss/**/*.scss', "!assets/scss/**/*.min.scss"])
+    .pipe(rename({suffix:".min"}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+  });
+
+gulp.task("watch", function() {
+    gulp.watch("assets/js/**/*.js", ["scripts"])
+    gulp.watch("assets/scss/**/*.scss", ["scss"])
+})
+
+gulp.task("default", ["scripts", "scss", "watch"]);
