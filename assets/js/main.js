0d8497f83929f75038e8bbe4375d003588021a4b
Bob Brown <robertbernardbrown@gmail.com>
2018-04-04 14:15:52 -0400

    adds browser sync to gulpfile and html watch task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 console.log("yoyoyo");
 console.log("you rock");
 console.log("poop");
-
 var headerNav = document.getElementsByClassName("header-nav")
 console.log(headerNav);
--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ var gulp = require("gulp");
     uglify = require("gulp-uglify");
     rename = require("gulp-rename");
     sass = require('gulp-sass');
+    autoprefixer = require("gulp-autoprefixer");
+    plumber = require("gulp-plumber");
+    browserSync = require("browser-sync");
+    reload = browserSync.reload;
 
 
 gulp.task("scripts", function(){
     gulp.src(["assets/js/**/*.js","!assets/js/**/*.min.js"])
+    .pipe(plumber())
     .pipe(rename({suffix:".min"}))
     .pipe(uglify())
-    .pipe(gulp.dest("assets/js"));
+    .pipe(gulp.dest("assets/js"))
+    .pipe(reload({stream:true}));
 })
 
 gulp.task('scss', function () {
@@ -16,12 +22,27 @@ gulp.task('scss', function () {
     .pipe(rename({suffix:".min"}))
     .pipe(sass().on('error', sass.logError))
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(reload({stream:true}));
   });
+
+gulp.task("html", function() {
+    gulp.src("**/*.html")
+    .pipe(reload({stream:true}));
+})
+
+gulp.task("browser-sync", function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    })
+})
 
 gulp.task("watch", function() {
     gulp.watch("assets/js/**/*.js", ["scripts"])
     gulp.watch("assets/scss/**/*.scss", ["scss"])
+    gulp.watch("**/*.html", ["html"])
 })
 
-gulp.task("default", ["scripts", "scss", "watch"]);
+gulp.task("default", ["scripts", "scss", "html", "browser-sync", "watch",]);
