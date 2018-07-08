28baf67577470af65768aacccc3912fc3370ecee
Bob Brown <robertbernardbrown@gmail.com>
2018-07-08 10:24:19 -0400

    small format change to portfolio

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Portfolio = (props) => (
     <div className="portfolio">
-    {props.projects.map((cur, i) => (
-        <div className="jumbotron header-div outline-red" key={i}>
-        <div className="container">
-            <p>{cur.title}</p>
-        </div>
-    </div>
-    ))}
+        {props.projects.map((cur, i) => (
+            <div className="jumbotron header-div outline-red" key={i}>
+                <div className="container">
+                    <p>{cur.title}</p>
+                </div>
+            </div>
+        ))}
     </div>
 )
 
