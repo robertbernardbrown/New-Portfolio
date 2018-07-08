96e9882df3777c0e86ee960a9b7f05c21edb12d9
Bob Brown <robertbernardbrown@gmail.com>
2018-07-08 11:47:44 -0400

    fills in project info and adds pictures

--- client/src/utils/projects.js
+++ client/src/utils/projects.js
@@ -1,27 +1,39 @@
 const projects = [
     {
-        title: "project 1",
-        description: "This was a project",
-        technology: "Mongo, Node, React",
-        picture: "standin.jpg"
+        title: "QuizCat",
+        description: "A timed, scheduled quiz game with rotating categories",
+        technology: "MongoDB, Express, React, Node, HTML/CSS",
+        picture: "https://static.vecteezy.com/system/resources/previews/000/192/828/large_2x/vector-abstract-landscape-illustration.jpg"
     },
     {
-        title: "project 2",
-        description: "This was a project",
-        technology: "Mongo, Node, React",
-        picture: "standin.jpg"
+        title: "Still-Open?",
+        description: "A completely front-end solution for finding restaurants open near you with alerts if they close soon.",
+        technology: "Vanilla JS, Jquery, HTML/CSS",
+        picture: "http://www.fantasysmacktalk.com/wp-content/uploads/2018/02/USATSI_10293240-340x270.jpg"
     },
     {
-        title: "project 3",
-        description: "This was a project",
-        technology: "Mongo, Node, React",
-        picture: "standin.jpg"
+        title: "Twinkle Site",
+        description: "A mocked e-commerce site for a local Savannah craft shop",
+        technology: "MySQL, Sequelize, Express, HTML/CSS, Node, jQuery",
+        picture: "http://www.fantasysmacktalk.com/wp-content/uploads/2018/02/USATSI_10293240-340x270.jpg"
     },
     {
-        title: "project 4",
-        description: "This was a project",
-        technology: "Mongo, Node, React",
-        picture: "standin.jpg"
+        title: "NYT React Site",
+        description: "A site to fetch articles from the NYT API given a topic and year parameters",
+        technology: "MongoDB, Express, React, Node, HTML/CSS",
+        picture: "http://www.fantasysmacktalk.com/wp-content/uploads/2018/02/USATSI_10293240-340x270.jpg"
+    },
+    {
+        title: "HackerRank Web-Scraper",
+        description: "A site for scraping the front page of HackerRank",
+        technology: "Cheerio, jQuery, HTML/CSS, Express, MongoDB, Mongoose, Node",
+        picture: "http://www.fantasysmacktalk.com/wp-content/uploads/2018/02/USATSI_10293240-340x270.jpg"
+    },
+    {
+        title: "Restaurant Bucketlist",
+        description: "A site to create a restaurant bucketlist.",
+        technology: "Express, MySQL, Node, jQuery, HTML/CSS",
+        picture: "http://www.fantasysmacktalk.com/wp-content/uploads/2018/02/USATSI_10293240-340x270.jpg"
     }
 ]
 
