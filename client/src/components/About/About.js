fd96dc600d739c8503c670547535e5d92747cf5c
Bob Brown <robertbernardbrown@gmail.com>
2018-07-10 15:20:32 -0400

    centers ul and li items with icons

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -6,16 +6,20 @@ const About = () => (
             <p>Hi there! My name is Bob Brown. I'm a fullstack developer looking for my next big challenge in frontend and backend design.</p>
         </div>
         <div className="container">
-            <p> Here are some of my skills:</p>
-            <ul className="fa-ul">
-                <li><i className="fa-li fa icon-mongodb"></i>MongoDB</li>
-                <li><i className="fa-li fa icon-reactjs"></i>React</li>
-                <li><i className="fa-li fa icon-nodejs"></i>Node</li>
-                <li><i className="fa-li fa icon-mysql-alt"></i>MySQL</li>
-                <li><i className="fa-li fa icon-python"></i>Python</li>
-                <li><i className="fa-li fa icon-javascript-alt"></i>Vanilla JS</li>
-                <li>Digital Marketing</li>
-            </ul>
+            <p>Here are some of my skills:</p>
+            <div id="icon-ul-div">
+                <ul className="fa-ul" id="flex-list">
+                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-mongodb"></i>MongoDB</li>
+                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-reactjs"></i>React</li>
+                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-nodejs"></i>Node</li>
+                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-mysql-alt"></i>MySQL</li>
+                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-python"></i>Python</li>
+                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-javascript-alt"></i>Vanilla JS</li>
+                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-bootstrap"></i>Bootstrap</li>
+                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-jquery"></i>jQuery</li>
+                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-line-graph"></i>Digital Marketing</li>
+                </ul>
+            </div>
         </div>
     </div>
 )
