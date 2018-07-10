4bcdb30f0de7f139fad27a24d807024947f5aa92
Bob Brown <robertbernardbrown@gmail.com>
2018-07-10 12:26:09 -0400

    adds class to about-me section

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const About = () => (
     <div className="jumbotron header-div" id="about">
-        <div className="container">
+        <div className="container about-me">
             <p>Hi there! My name is Bob Brown. I'm a fullstack developer looking for my next big challenge in frontend and backend design.</p>
         </div>
         <div className="container">
-            <p>Here are some of my skills:</p>
+            <p> Here are some of my skills:</p>
             <ul className="fa-ul">
                 <li><i className="fa-li fa icon-mongodb"></i>MongoDB</li>
                 <li><i className="fa-li fa icon-reactjs"></i>React</li>
