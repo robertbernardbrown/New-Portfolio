431b5e7c72b563231a7f660b6eb044c0043bace2
Bob Brown <robertbernardbrown@gmail.com>
2018-07-11 10:53:10 -0400

    adds styling to portfolio containers to fade-in on hover

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -6,13 +6,17 @@ const Portfolio = (props) => (
         <h1>Portfolio</h1>
         <CardColumns>
         {props.projects.map((cur, i) => (
-            <a href={cur.url} target="_blank" rel="noopener noreferrer" key={i}>
+            <a href={cur.url} target="_blank" rel="noopener noreferrer" key={i} className="portfolio-container">
                 <Card inverse>
                     <CardImg width="100%" src={cur.picture} alt="Card image cap"/>
-                    <CardImgOverlay>
-                    <CardTitle>{cur.title}</CardTitle>
-                    <CardText>{cur.description}</CardText>
-                    </CardImgOverlay>
+                        <CardImgOverlay>
+                            <CardTitle>{cur.title}</CardTitle>
+                        </CardImgOverlay>
+                    <div className="portfolio-overlay">
+                        <CardImgOverlay>
+                            <CardText className="text">{cur.description}</CardText>
+                        </CardImgOverlay>
+                    </div>
                 </Card>
             </a>
         ))}
