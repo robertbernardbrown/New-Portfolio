cdadef222f3d34c7d7de0a22d5e82e1c73e0446e
Bob Brown <robertbernardbrown@gmail.com>
2018-07-12 14:43:56 -0400

    removes unnecessary files and adds scroll listening for css animations

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -4,22 +4,23 @@ const About = () => (
     <div className="header-div hero" id="about">
         <div className="container about-me">
             <h1>Hi there!</h1> 
-            <p>Apologies if you were looking for <a href="https://en.wikipedia.org/wiki/Bobby_Brown" target="_blank">Bobby Brown</a> (of New Edition fame) or <a href="https://en.wikipedia.org/wiki/Robert_Brown_(botanist,_born_1773)" target="_blank">Robert Brown</a> (reknowned botanist), but I'm not your guy.</p>
-            <p>However, if you're looking or a fullstack developer I'm totally your guy.</p>
+            <hr/>
+            <p>Apologies if you're looking for <a href="https://en.wikipedia.org/wiki/Bobby_Brown" target="_blank" rel="noopener noreferrer">Bobby Brown</a> (of New Edition fame) or <a href="https://en.wikipedia.org/wiki/Robert_Brown_(botanist,_born_1773)" target="_blank" rel="noopener noreferrer">Robert Brown</a> (reknowned botanist), but I'm not your guy.</p>
+            <p>However, if you're looking for a fullstack developer I'm <strong>totally</strong> your guy.</p>
         </div>
         <div className="container">
             <p>Here are some of my skills:</p>
             <div id="icon-ul-div">
                 <ul className="fa-ul" id="flex-list">
-                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-mongodb"></i>MongoDB</li>
-                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-reactjs"></i>React</li>
-                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-nodejs"></i>Node</li>
-                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-mysql-alt"></i>MySQL</li>
-                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-python"></i>Python</li>
-                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-javascript-alt"></i>Vanilla JS</li>
-                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-bootstrap"></i>Bootstrap</li>
-                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-jquery"></i>jQuery</li>
-                    <li className="icon-list-item"> <i className="list-icon fa-li fa icon-line-graph"></i>Digital Marketing</li>
+                    <li className="icon-list-item"> <div><i className="list-icon fa-li fa icon-mongodb"></i></div><div>MongoDB</div></li>
+                    <li className="icon-list-item"> <div><i className="list-icon fa-li fa icon-reactjs"></i></div><div>React</div></li>
+                    <li className="icon-list-item"> <div><i className="list-icon fa-li fa icon-nodejs"></i></div><div>Node</div></li>
+                    <li className="icon-list-item"> <div><i className="list-icon fa-li fa icon-mysql-alt"></i></div><div>MySQL</div></li>
+                    <li className="icon-list-item"> <div><i className="list-icon fa-li fa icon-python"></i></div><div>Python</div></li>
+                    <li className="icon-list-item"> <div><i className="list-icon fa-li fa icon-javascript-alt"></i></div><div>Vanilla JS</div></li>
+                    <li className="icon-list-item"> <div><i className="list-icon fa-li fa icon-bootstrap"></i></div><div>Bootstrap</div></li>
+                    <li className="icon-list-item"> <div><i className="list-icon fa-li fa icon-jquery"></i></div><div>jQuery</div></li>
+                    <li className="icon-list-item"> <div><i className="list-icon fa-li fa icon-line-graph"></i></div><div>Digital Marketing</div></li>
                 </ul>
             </div>
         </div>
--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -3,30 +3,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./Main.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-// import NavbarComp from "../../components/Navbar/Navbar";
 import About from "../../components/About/About";
-import Contact from "../../components/Contact/Contact";
 import Portfolio from "../../components/Portfolio/Portfolio";
 import projects from "../../utils/projects";
 
-console.log(projects);
-
 class Main extends Component{
 
-    state = {
-        place: 0
+    componentDidMount(){
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = (e) => {
+        const aboutHeight = e.target.body.clientHeight <= window.scrollY
+        if (aboutHeight) {
+            window.removeEventListener('scroll', this.handleScroll);
+        }
     }
 
     render(){
         return(
             <div className="site-container">
                 <Header/>
-                <About/>
+                <About onScroll={this.handleScroll}/>
                 <Portfolio projects={projects}/>
-                {/* <Contact/> */}
                 <Footer/>
             </div>
-
         )
     }
 }
