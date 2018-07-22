909bc1a8e3b60ee2fd997c9567e60ed1a5a3e37e
Bob Brown <robertbernardbrown@gmail.com>
2018-07-22 12:03:47 -0400

    adds https server

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const express   = require("express");
+const path      = require("path");
+const PORT      = process.env.PORT || 3001;
+const HTTPSPORT = process.env.PORT || 5000;
+const app       = express();
+var http        = require('http');
+var https       = require('https');
+var fs          = require('fs');
 
-// Serve up static assets (usually on heroku)
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
+var options = {
+  key: fs.readFileSync('conf/key.pem'),
+  cert: fs.readFileSync('conf/cert.pem'),
+};
+
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+http.createServer(app).listen(PORT, function(){
   console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
+
+https.createServer(options, app).listen(HTTPSPORT, function(){
+  console.log(`🌎 ==> Server now on port ${HTTPSPORT}!`);
 });
